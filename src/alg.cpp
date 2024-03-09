8dc2f31cc9840eb6c40c58ece9952253764469e6
Ekaterina Pokatova <150853636+EkaterinaPokatova@users.noreply.github.com>
2024-03-09 21:36:46 +0300

    Pokatova

--- src/alg.cpp
+++ src/alg.cpp
@@ -43,7 +43,7 @@ double sinn(double x, uint16_t count) {
 }
 
 double cosn(double x, uint16_t count) {
-    double result = 0;
+    double result = 1;
     for (uint64_t i = 1; i <= count; i++) {
         result += pown(-1, i - 2) * calcItem(x, (2 * i) - 2);
     }
